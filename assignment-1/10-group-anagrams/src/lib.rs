d8e7a3895db5e7c84478006c2237361cb647fb4d
Saishreeta <ece.24becj35@silicon.ac.in>
2026-06-29 10:28:36 +0530

    Update lib.rs

--- assignment-1/10-group-anagrams/src/lib.rs
+++ assignment-1/10-group-anagrams/src/lib.rs
@@ -1,6 +1,14 @@
+use std::collections::HashMap;
+
 pub fn group_anagrams(words: &[String]) -> Vec<Vec<String>> {
-    let _ = words;
-    todo!("implement group_anagrams")
+    let mut groups: HashMap<String, Vec<String>> = HashMap::new();
+    for word in words {
+        let mut sorted_chars: Vec<char> = word.to_lowercase().chars().collect();
+        sorted_chars.sort_unstable();
+        let sorted_key: String = sorted_chars.into_iter().collect();
+        groups.entry(sorted_key).or_default().push(word.clone());
+    }
+    groups.into_values().collect()
 }
 
 #[cfg(test)]
